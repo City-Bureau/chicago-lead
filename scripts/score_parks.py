999544bd8fffff66e63d7eec83277a10ccd336dc
Hannah Cushman <Hannah@Hannahs-MacBook-Pro.local>
2016-12-14 15:30:19 -0600

    revises scoring

--- scripts/score_parks.py
+++ scripts/score_parks.py
@@ -7,7 +7,7 @@ df = pd.read_csv(sys.argv[1])
 df['processed_result'] = pd.to_numeric(df['result'], errors='coerce')
 df.fillna(0, inplace=True)
 
-df['exceeds_epa'] = df['processed_result'].apply(lambda x: 0 if x < 15 else 1)
+df['exceeds_epa'] = df['processed_result'].apply(lambda x: 0 if x <= 15 else 1)
 
 pd.DataFrame(
     {'score': df.groupby(['park_name'])['exceeds_epa'].mean(),
