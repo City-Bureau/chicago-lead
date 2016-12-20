b3f9670f196b7a402b384b497154f7258486ce9c
Hannah Cushman <Hannah@Hannahs-MacBook-Pro.local>
2016-12-20 09:46:27 -0600

    water_projects.mk now functional!

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-all : output/cps_lead.geojson output/parks_lead_scores.csv clean 
+all : output/cps_lead.geojson output/parks_lead_scores.csv output/water_projects_all_years.geojson clean 
 
 include schools.mk parks.mk water_projects.mk
 
-clean : clean_schools clean_parks
+clean : clean_schools clean_parks clean_water_projects
--- water_projects.mk
+++ water_projects.mk
@@ -5,8 +5,14 @@ setupdb :
 	make $(PG_DB)
 	make water_projects
 
+$(PG_DB) :
+	createdb $@
+	psql -d $@ -c "CREATE EXTENSION POSTGIS"
+
 water_projects : raw/water_projects.geojson
 	ogr2ogr -f PostgreSQL PG:"dbname=$(PG_DB)" -nln $@ $<
+	psql -d $(PG_DB) -c "alter table water_projects alter column project_submit_date type bigint using (project_submit_date::bigint), \
+	alter column enddate type bigint using (enddate::bigint), alter column startdate type bigint using (startdate::bigint)"
 	psql -d $(PG_DB) -c "alter table water_projects alter column project_submit_date type timestamp using timestamp 'epoch' + project_submit_date * interval '1 millisecond'"
 	psql -d $(PG_DB) -c "alter table water_projects alter column enddate type timestamp using timestamp 'epoch' + enddate * interval '1 millisecond'"
 	psql -d $(PG_DB) -c "alter table water_projects alter column startdate type timestamp using timestamp 'epoch' + startdate * interval '1 millisecond'"
@@ -14,6 +20,9 @@ water_projects : raw/water_projects.geojson
 foia : raw/WaterCIP2016_20161121.xlsx
 	in2csv $< |\
 	csvsql --db postgresql:///$(PG_DB) --table $@ --insert
+
+output/water_projects_all_years.geojson : setupdb
+	ogr2ogr -f GeoJSON $@ "PG:dbname=$(PG_DB)"
 
 2011_projects.geojson :
 	ogr2ogr -f GeoJSON $@ "PG:dbname=$(PG_DB)" \
@@ -39,9 +48,6 @@ foia : raw/WaterCIP2016_20161121.xlsx
 	ogr2ogr -f GeoJSON $@ "PG:dbname=$(PG_DB)" \
             -sql "SELECT * FROM water_projects WHERE startdate < '2017-01-01'::DATE AND (enddate IS NULL or enddate >= '2016-01-01'::DATE)"
 
-water_projects_all_years.geojson :
-	ogr2ogr -f GeoJSON $@ "PG:dbname=$(PG_DB)"
-
-$(PG_DB) :
-	createdb $@
-	psql -d $@ -c "CREATE EXTENSION POSTGIS"
+.PHONY : clean_water_projects
+clean_water_projects :
+	dropdb $(PG_DB)
