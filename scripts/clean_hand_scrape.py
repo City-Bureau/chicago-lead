397b731fde1e12b7d0f6f09ab799b4fc1c91f816
Hannah Cushman <Hannah@Hannahs-MacBook-Pro.local>
2016-12-22 17:03:03 -0600

    tabs to spaces & deletes pandas processing script

--- scripts/clean_hand_scrape.py
+++ scripts/clean_hand_scrape.py
@@ -1,53 +1,53 @@
 import re
 
 def open_file(file_name, skiprows=1):
-	with open(file_name) as f:
-		for i in range(0, skiprows):
-			next(f)
-		return f.read()
+    with open(file_name) as f:
+        for i in range(0, skiprows):
+            next(f)
+        return f.read()
 
 def strip_nl(x):
-	base = x.group(0).split('\n')[0]
-	if ',' in base:
-		return '%s,' % base
-	return '%s,' % base.strip('"')
+    base = x.group(0).split('\n')[0]
+    if ',' in base:
+        return '%s,' % base
+    return '%s,' % base.strip('"')
 
 def detect_start(x):
-	return '\n' if '\n' in x.group(0) else ''
+    return '\n' if '\n' in x.group(0) else ''
 
 def strip_flags(x):
-	return re.sub(',(-|BA|AA|ND)\n', '\n', x)
+    return re.sub(',(-|BA|AA|ND)\n', '\n', x)
 
 def convert_none(x):
-	return re.sub('(ND|<[0-9].[0-9]+|None Detected)', '0', x)
+    return re.sub('(ND|<[0-9].[0-9]+|None Detected)', '0', x)
 
 def prepend_school_name(x, school_name):
-	x = re.sub('^', '%s,' % school_name, x)
-	return re.sub('\n', '\n%s,' % school_name, x)
+    x = re.sub('^', '%s,' % school_name, x)
+    return re.sub('\n', '\n%s,' % school_name, x)
 
 def append_file_name(x, filename):
-	pdf = re.sub('.csv', '.pdf', filename.split('-')[-1])
-	return re.sub('\n', ',%s\n' % pdf, x)
+    pdf = re.sub('.csv', '.pdf', filename.split('-')[-1])
+    return re.sub('\n', ',%s\n' % pdf, x)
 
 def process(x, school_name, file_name, droprows=1):
-	x = strip_flags(x)
-	x = convert_none(x)
-	x = prepend_school_name(x, school_name)
-	x = append_file_name(x, file_name)
-	return '\n'.join(x.split('\n')[:-droprows])
+    x = strip_flags(x)
+    x = convert_none(x)
+    x = prepend_school_name(x, school_name)
+    x = append_file_name(x, file_name)
+    return '\n'.join(x.split('\n')[:-droprows])
 
 files = {
-	'Sabin': 'tabula/tabula-individualschool_sabin_610342.csv', 
-	'Powell': 'tabula/tabula-IndividualSchool_Powell_610281.csv', 
-	'Falconer': 'tabula/tabula-Individualschool_Falconer_609910.csv', 
-	'Skinnernorth': 'tabula/tabula-individualschool_skinnernorth_610534.csv', 
-	'Tanner': 'tabula/tabula-Individualschool_Tanner_610279.csv', 
-	'Younghs': 'tabula/tabula-IndividualSchool_YoungHS_ 609755.csv', 
-	'Juarezhs': 'tabula/tabula-IndividualSchool_JuarezHS_ 609764.csv', 
-	'Garvy': 'tabula/tabula-individualschool_garvy_609937.csv', 
-	'Cassell': 'tabula/tabula-individualschool_cassell_609849.csv', 
-	'Collinshs': 'tabula/tabula-IndividualSchool_CollinsHS_610499.csv', 
-	'Lasalle': 'tabula/tabula-individualschool_lasalle_610033.csv'
+    'Sabin': 'tabula/tabula-individualschool_sabin_610342.csv', 
+    'Powell': 'tabula/tabula-IndividualSchool_Powell_610281.csv', 
+    'Falconer': 'tabula/tabula-Individualschool_Falconer_609910.csv', 
+    'Skinnernorth': 'tabula/tabula-individualschool_skinnernorth_610534.csv', 
+    'Tanner': 'tabula/tabula-Individualschool_Tanner_610279.csv', 
+    'Younghs': 'tabula/tabula-IndividualSchool_YoungHS_ 609755.csv', 
+    'Juarezhs': 'tabula/tabula-IndividualSchool_JuarezHS_ 609764.csv', 
+    'Garvy': 'tabula/tabula-individualschool_garvy_609937.csv', 
+    'Cassell': 'tabula/tabula-individualschool_cassell_609849.csv', 
+    'Collinshs': 'tabula/tabula-IndividualSchool_CollinsHS_610499.csv', 
+    'Lasalle': 'tabula/tabula-individualschool_lasalle_610033.csv'
 }
 
 # special cases
@@ -100,9 +100,9 @@ lasalle = open_file(files['Lasalle'])
 lasalle = process(lasalle, 'Lasalle', files['Lasalle'])
 
 data = ('\n'.join([
-	'school,sample,location,date,result,filename',
-	collinshs, juarezhs, tanner, younghs, falconer, 
-	sabin, powell, skinnernorth, garvy, cassell, lasalle
+    'school,sample,location,date,result,filename',
+    collinshs, juarezhs, tanner, younghs, falconer, 
+    sabin, powell, skinnernorth, garvy, cassell, lasalle
 ]))
 
 data = re.sub(',,', ',', data)
