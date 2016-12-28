6a17696fa31d595f2f6b59d6ef6a3866c2efd461
Hannah Cushman <Hannah@Hannahs-MacBook-Pro.local>
2016-12-28 14:19:00 -0600

    refactors sed & psql out of parks.mk

--- parks.mk
+++ parks.mk
@@ -1,25 +1,16 @@
-PG_DB = parks_lead
-
-$(PG_DB) : 
-	createdb $(PG_DB)
-
 clean_scrape.csv : tabula/tabula-Water_Fountain_Testing_Final_Results.csv
-	sed -e '1s/^/fixture_location,result=/' $< | tr '=' '\n' | \
-	sed -e 's/^\("",\)*//' -e 's/\(,""\)\{2,\}/,""/' -e 's/,"",/,/' -e 's/ ,/,/' | \
-	sed -e 's/[0-9] *[UJ]//' -e 's/[<˂][0-9?.0-9]*/0/' | \
+	perl -p -e 'print "fixture_location,result\n" if $$. == 1' $< | \
+	perl -p -e 's/^("",)*//; s/(,""){2,}/,""/; s/,"",/,/; s/ ,/,/' | \
+	perl -p -e 's/\s*[UJ].{1,2}$$//; s/˂/</; s/<[0-9]..[0-9]*/0/' | \
 	csvsql --query "select * from stdin where fixture_location not like '%Follow Up%' \
 										  and fixture_location not like '%repair%' \
 										  and fixture_location not like '%Test 2%' \
-										  and fixture_location not in ('Indoor', 'Outdoor', 'Oudoor')" | \
+										  and fixture_location not in ('Indoor', 'Outdoor', 'Oudoor')"| \
 	python scripts/pivot_park_headers.py > $@
 
-parks_lead_scores.csv : clean_scrape.csv $(PG_DB)
-	csvsql --db postgresql:///$(PG_DB) --insert --table "lead_scores" --blanks $<
-	psql -d $(PG_DB) -c "alter table lead_scores add exceeds_epa int"
-	psql -d $(PG_DB) -c "update lead_scores set exceeds_epa = case when result <= 15 then 0 else 1 end;"
-	psql -d $(PG_DB) -c "\copy (select park_name, count(*) as num_fixtures, round(avg(exceeds_epa), 4) as score from lead_scores group by park_name order by park_name) to 'output/$@' csv header"
+parks_lead_scores.csv : clean_scrape.csv
+	cat $< | python scripts/parks_score.py > output/$@
 
 .PHONY : clean_parks
 clean_parks :
 	rm clean_scrape.csv
-	dropdb $(PG_DB)
---
+++ scripts/parks_score.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+import itertools
+
+reader = csv.reader(sys.stdin)
+writer = csv.writer(sys.stdout)
+
+next(reader)
+writer.writerow(['park_name', 'score', 'num_fixtures'])
+
+for park_name, measurements in itertools.groupby(reader, lambda x: x[0]):
+	measurements = list(measurements)
+	count = sum(float(result) > 15 for _, _, result in measurements)
+	num_fixtures = len(measurements)
+	score = count / num_fixtures
+	writer.writerow([park_name, score, num_fixtures])
