bb5950cacec3d72247b5f3f099ee991bded2e525
Hannah Cushman <Hannah@Hannahs-MacBook-Pro.local>
2016-12-29 14:49:47 -0600

    one more spaces to tabs

--- scripts/parks_score.py
+++ scripts/parks_score.py
@@ -9,8 +9,8 @@ next(reader)
 writer.writerow(['park_name', 'score', 'num_fixtures'])
 
 for park_name, measurements in itertools.groupby(reader, lambda x: x[0]):
-	measurements = list(measurements)
-	count = sum(float(result) > 15 for _, _, result in measurements)
-	num_fixtures = len(measurements)
-	score = count / num_fixtures
-	writer.writerow([park_name, score, num_fixtures])
+    measurements = list(measurements)
+    count = sum(float(result) > 15 for _, _, result in measurements)
+    num_fixtures = len(measurements)
+    score = count / num_fixtures
+    writer.writerow([park_name, score, num_fixtures])
