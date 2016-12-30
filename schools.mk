8c15d69a33b0c8554af7529b7ff5cf3234d35527
Forest Gregg <fgregg@uchicago.edu>
2016-12-30 09:59:42 -0600

    move most of the school processing to get-the-lead-out module

--- schools.mk
+++ schools.mk
@@ -1,29 +1,15 @@
 .PHONY: schools
 schools : cps_lead_scores.csv cps_lead.geojson
 
-hand_scrape.clean.csv : 
-	python scripts/clean_hand_scrape.py > $@
+.INTERMEDIATE : fusion.csv
+fusion.csv : raw/cps_lead_fusion_table.csv
+	perl -p -e 's/Individualschool_Falconer_609910/Individualschool_Falconer_609910.pdf/' $< | \
+	perl -p -e 's/4,LEAD FOUND,IndividualSchool_Williams_610380.pdf/4,LEAD FOUND,None/' > $@
 
-tabula.clean.csv : tabula/tabula-cps_lead_results.csv
-	perl -p -e 's|,pdf.$$|,filename|; s|http:.*LeadTesting/||' $< > $@.tmp
-	echo "\nZapata,1-E-CS02-51,Room 105 - North,6/3/16 6:50 AM,27.6,Individualschool_Zapata_609973.pdf\n\
-	Orr,51558-1-HAL-F05,\"Main- Next to Room 118, Fountain\",10/12/16 6:00 AM,530,IndividualSchool_Orr_610389.pdf" >> $@.tmp
-	csvsort $@.tmp | python scripts/clean_tabula.py > $@
+output/cps_lead_scores.csv : get-the-lead-out/cps.csv
+	cat $< | python scripts/cps_score.py > $@
 
-fusion.clean.csv : raw/cps_lead_fusion_table.csv
-	perl -p -e 's|Individualschool_Falconer_609910|Individualschool_Falconer_609910.pdf|;\
-	s|4,LEAD FOUND,IndividualSchool_Williams_610380.pdf|4,LEAD FOUND,None|' $< > $@
- 
-output/cps_lead_scores.csv : hand_scrape.clean.csv tabula.clean.csv fusion.clean.csv
-	csvstack $< $(word 2,$^) | csvsql --query "select distinct SchoolName, sample, location, date, result, s.filename \
-	from '$(basename $(word 3,$^))' as f left join stdin as s on (f.Filename=s.filename)" --no-inference $(word 3,$^) | \
-	python scripts/cps_score.py > $@
-
-output/cps_lead.geojson : output/cps_lead_scores.csv fusion.clean.csv
-	csvsql --query "select s.school_name, s.score, s.num_fixtures, f.lat, f.long from $(notdir $(basename $<)) as s \
-	join '$(basename $(word 2,$^))' as f on (upper(s.school_name)=f.SchoolName)" --no-inference $< $(word 2,$^) | \
+output/cps_lead_scores.geojson : output/cps_lead_scores.csv fusion.csv
+	csvjoin -c "filename","Filename" $^ | \
 	csvjson --lat Lat --lon Long --crs urn:ogc:def:crs:OGC:1.3:CRS84 --indent 2  > $@
 
-.PHONY : clean_schools
-clean_schools :
-	rm *.clean.csv *.tmp
--- scripts/cps_score.py
+++ scripts/cps_score.py
@@ -6,21 +6,19 @@ reader = csv.reader(sys.stdin)
 writer = csv.writer(sys.stdout)
 
 next(reader)
-writer.writerow(['school_name', 'score', 'num_fixtures'])
 
-for school_name, measurements in itertools.groupby(reader, lambda x: x[0]):
+writer.writerow(['school_name', 'score', 'num_fixtures', 'filename'])
+
+for school, measurements in itertools.groupby(reader, lambda x: x[0]):
     fixture_scores = []
-    school_name = school_name.title()
     for fixture, measurements in itertools.groupby(measurements, lambda x: '-'.join(x[1].split('-')[:-1])):
         fixture_measurements = list(measurements)
         num_trials = len(fixture_measurements)
-        try:
-            count = sum(float(result) > 15 for _, _, _, _, result, _ in fixture_measurements)
-        except ValueError:
-            continue
+        count = sum(float(result) > 15 for _, _, _, _, result, _ in fixture_measurements)
         score = count / num_trials
         fixture_scores.append(score)
-    if not fixture_scores:
-        writer.writerow([school_name, None, None])
-        continue
-    writer.writerow([school_name, sum(fixture_scores)/len(fixture_scores), len(fixture_scores)])
+
+    writer.writerow([school.title(),
+                     sum(fixture_scores)/len(fixture_scores),
+                     len(fixture_scores),
+                     fixture_measurements[-1][-1]])
