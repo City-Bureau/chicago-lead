42730ce04effa97751bdb741eddc3bded17009ea
Forest Gregg <fgregg@uchicago.edu>
2016-12-29 21:59:47 -0600

    tabula setup for parks.mk

--- parks.mk
+++ parks.mk
@@ -1,6 +1,17 @@
+tabula = java -jar ./tabula-java/target/tabula-0.9.1-jar-with-dependencies.jar
+
+tabula-java :
+	git clone https://github.com/tabulapdf/tabula-java.git
+	cd tabula-java && mvn clean compile assembly:single
+
+.INTERMEDIATE : tabula/parks.csv
+tabula/parks.csv : raw/Water_Fountain_Testing_Final_Results.pdf tabula-java
+	$(tabula) -p all -g $< > $@
+
 .INTERMEDIATE : park_fixtures.csv
-park_fixtures.csv : tabula/tabula-Water_Fountain_Testing_Final_Results.csv
-	perl -p -e 'print "fixture_location,result\n" if $$. == 1' $< | \
+park_fixtures.csv : tabula/parks.csv
+	tail -n +7 $< | \
+	perl -p -e 'print "fixture_location,result\n" if $$. == 1' | \
 	perl -p -e 's/^("",)*//' | \
         perl -p -e 's/(,""){2}/,""/' | \
         perl -p -e 's/,"",/,/' | \
