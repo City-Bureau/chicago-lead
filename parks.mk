a4e48725aa3e35cb59af991128873d3a7e648c64
Forest Gregg <fgregg@uchicago.edu>
2016-12-29 22:03:45 -0600

    revert cleaning

--- parks.mk
+++ parks.mk
@@ -9,8 +9,7 @@ tabula/parks.csv : raw/Water_Fountain_Testing_Final_Results.pdf tabula-java
 
 .INTERMEDIATE : park_fixtures.csv
 park_fixtures.csv : tabula/parks.csv
-	tail -n +7 $< | \
-	perl -p -e 'print "fixture_location,result\n" if $$. == 1' | \
+	perl -p -e 'print "fixture_location,result\n" if $$. == 1' $< | \
 	perl -p -e 's/^("",)*//' | \
         perl -p -e 's/(,""){2}/,""/' | \
         perl -p -e 's/,"",/,/' | \
