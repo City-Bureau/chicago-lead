96e82ba9a20b12b9ef52c6f3e39117c33e81808e
Forest Gregg <fgregg@uchicago.edu>
2016-12-29 21:39:14 -0600

    remove Oudoor

--- parks.mk
+++ parks.mk
@@ -7,7 +7,7 @@ park_fixtures.csv : tabula/tabula-Water_Fountain_Testing_Final_Results.csv
         perl -p -e 's/ ,/,/' | \
 	perl -p -e 's/\s*[UJ].{1,2}$$//' | \
         perl -p -e 's/˂/</' | \
-        csvgrep -c fixture_location -r ".*(Follow Up|repair|Test 2|Indoor|Outdoor).*" -i | \
+        csvgrep -c fixture_location -r ".*(Follow Up|repair|Test 2|Indoor|Outdoor|Oudoor).*" -i | \
 	python scripts/pivot_park_headers.py > $@
 
 output/parks_lead_scores.csv : park_fixtures.csv
