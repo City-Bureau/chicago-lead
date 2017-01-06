65bc9b67f82794a3e71d495c12f3581ee3bbe837
Forest Gregg <fgregg@uchicago.edu>
2017-01-06 13:47:47 -0600

    update cps scoring to handle truncated values

--- scripts/cps_score.py
+++ scripts/cps_score.py
@@ -5,6 +5,21 @@ import itertools
 reader = csv.reader(sys.stdin)
 writer = csv.writer(sys.stdout)
 
+def above_threshold(result):
+    if '<' in result:
+        return False
+    elif 'None Detected' in result:
+        return False
+    elif result == '':
+        return float('nan')
+    else:
+        try:
+            return float(result) > 15
+        except:
+            print('foo', result)
+            raise
+    
+
 next(reader)
 
 writer.writerow(['school_name', 'score', 'num_fixtures', 'filename'])
@@ -12,9 +27,10 @@ writer.writerow(['school_name', 'score', 'num_fixtures', 'filename'])
 for school, measurements in itertools.groupby(reader, lambda x: x[0]):
     fixture_scores = []
     for fixture, measurements in itertools.groupby(measurements, lambda x: '-'.join(x[1].split('-')[:-1])):
-        fixture_measurements = list(measurements)
+        fixture_measurements = [each for each in measurements if each != '']
         num_trials = len(fixture_measurements)
-        count = sum(float(result) > 15 for _, _, _, _, result, _ in fixture_measurements)
+        count = sum(above_threshold(result)
+                    for _, _, _, _, result, _ in fixture_measurements)
         score = count / num_trials
         fixture_scores.append(score)
 
